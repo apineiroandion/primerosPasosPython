bc786319cd2d5d1ed351d9ad03eeddbf6913119b
apineiroandion <apineiroandion@danielcastelao.org>
2024-09-24 13:11:19 +0200

    mas metodos

--- Metodos.py
+++ Metodos.py
@@ -23,4 +23,33 @@ def saudar (nome = "Mundo"):
 saudar()
 saudar("Pepe")
 
+## Metodos con parametros indeterminados
 
+def varios_parametros (parametro1, parametro2, *otros):
+    print(parametro1)
+    print(parametro2)
+    print(otros)
+
+varios_parametros(1, 2, 3, 4, 5)
+
+def suma_varios (*numeros):
+    suma = 0
+    for numero in numeros:
+        suma += numero
+    return suma
+
+print(suma_varios(1, 2, 3, 4, 5))
+
+def varios_parametros_non_anonimos (parametro1, parametro2, **otros):
+    print(parametro1)
+    print(parametro2)
+    print(otros)
+
+varios_parametros_non_anonimos(1, 2, terceiro = 3, cuarto = 4, quinto = 5)
+
+## podense retornar varios valores
+def varios_valores ():
+    return 1, 2, 3, 4, 5
+a, b, c, d, e = varios_valores()
+
+print(a, b, c, d, e)
